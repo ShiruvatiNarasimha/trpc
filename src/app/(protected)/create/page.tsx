9798402cbd142600990cb52fe4b2710d879735bf
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-02 03:33:14 +0530

    Updated code

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+"use client";
 
-const Page = () => {
-  return <div>Hey Thee I am Project</div>;
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { api } from "@/trpc/react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+type FormInput = {
+  repoUrl: string;
+  projectName: string;
+  githubToken?: string;
 };
 
-export default Page;
+const CreatePage = () => {
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+  const createProject = api.project.createProject.useMutation();
+
+  function onSubmit(data: FormInput) {
+    createProject.mutate(
+      {
+        githubUrl: data.repoUrl,
+        name: data.projectName,
+        githubToken: data.githubToken,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Project created successfully");
+          reset();
+        },
+        onError: () => {
+          toast.error("Failed to create project");
+        },
+      },
+    );
+    return true;
+  }
+  return (
+    <div className="flex h-full items-center justify-center gap-12">
+      <img src="/undraw_github.svg" className="h-56 w-auto" />
+      <div>
+        <div>
+          <h1 className="text-2xl font-semibold">
+            Link your GitHub Repository
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Enter the URL of your repository to link it to GitHub.AI
+          </p>
+        </div>
+        <div className="h-4"></div>
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              {...register("projectName", { required: true })}
+              placeholder="Project Name"
+              required
+            />
+            <div className="h-2"></div>
+            <Input
+              {...register("repoUrl", { required: true })}
+              placeholder="Github URL"
+              type="url"
+              required
+            />
+            <div className="h-2"></div>
+            <Input
+              {...register("githubToken")}
+              placeholder="Github Token (Optional)"
+            />
+            <div className="h-4"></div>
+            <Button type="submit" disabled={createProject.isPending}>
+              Create Project
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
