2273cc92637ba8129c0fe9b5ed0119138def0719
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-02-02 22:24:35 +0530

    Building UI dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,13 +2,44 @@
 
 import useProject from "@/hooks/use-project";
 import { useUser } from "@clerk/nextjs";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const DashboardPage = () => {
   const { project } = useProject();
   return (
     <div>
-      <h1>{project?.name}</h1>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        <div className="w-fit rounded-md bg-primary px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is linked to {""}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-center text-white/80 hover:underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="h-4"></div>
+        <div className="flex items-center gap-4">
+          TeamMembers InviteButton ArchiveButton
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard MeetingCard
+        </div>
+      </div>
+      <div className="mt-8">commitLogo</div>
     </div>
   );
 };
